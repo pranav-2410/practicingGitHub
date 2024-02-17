699ca8c483e4e4a69d4bc156b56bfd666a7ccaa7
Pranav P <pranav0024p@gmail.com>
2024-02-17 18:01:42 +0530

    JavaPractice class is modified

--- JavaPractice.java
+++ JavaPractice.java
@@ -1,16 +1,20 @@
 import java.util.*;
 import java.lang.*;
-import java.util.stream.Collectors;
 
 public class JavaPractice {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
         System.out.println("Hello there");
         List<String> names = new ArrayList<>();
         names.add("Pranav");
         names.add("Kiran");
 
-        List<String> editednames= names.stream().map(name->name+"1").toList();
-        System.out.println(editednames);
+        List<String> editedNames= names.stream().map(name->name+"1").toList();
+        System.out.println(editedNames);
+        JavaPractice.printYourName(names);
+    }
+
+    public static void printYourName(List<String> names){
+        System.out.println(names);
     }
 }
